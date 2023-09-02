ad25f0f61b49664c8c10bf1f097e17fc12fea8aa
malexa21 <aaliroalexandra@gmail.com>
2023-09-02 14:13:39 +0300

    Exercitiu 3: Destructurarea

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-//const suma = (a,b) => {
-//    return a+b
-//};
+//Arrow Functions
+const add = (a,b) => {
+    return a+b
+};
 
-//console.log(suma(10 ,20));
+console.log(add(10 ,20));
 
+
+//HOF
 const suma = (a,b) => (a+b);
 const dif = (a,b) => (a-b);
 const mult = (a,b) => (a*b);
@@ -14,7 +17,23 @@ const operatiiMatematice = (a, b, func) => func(a,b);
 
 const n = operatiiMatematice(10 , 15 ,suma)
 
-console.log(n)
+console.log(n);
 
+
+//Destructurarea
+
+const obiect = {
+    prop1:5,
+    prop2:10,
+    prop3:7,
+    prop4:[4,5,6],
+    prop5:{x:"mau" , b:"mar",},
+};
+
+const {prop1,prop2,prop3,prop4} = obiect;
+
+const sum =prop1 + prop2 + prop3 + prop4[1];
+
+console.log(sum);
 
 
