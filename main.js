a4f7e54c1bea68f0abec49f03f21f7e2e4643bc9
malexa21 <aaliroalexandra@gmail.com>
2023-09-02 13:48:34 +0300

    Exercitiu 2: HOF

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-const suma = (a,b) => {
-    return a+b
-};
+//const suma = (a,b) => {
+//    return a+b
+//};
 
-console.log(suma(10 ,20))
+//console.log(suma(10 ,20));
+
+const suma = (a,b) => (a+b);
+const dif = (a,b) => (a-b);
+const mult = (a,b) => (a*b);
+const div = (a,b) => (a/b);
+const mod = (a,b) => (a%b);
+
+const operatiiMatematice = (a, b, func) => func(a,b);
+
+const n = operatiiMatematice(10 , 15 ,suma)
+
+console.log(n)
+
+
+
